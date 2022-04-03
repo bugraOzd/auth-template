b41d13071082169ddafbb388f4049c956a1395c9
bugra <bugraozdilek@gmail.com>
2022-04-03 18:15:52 +0300

    delete unused imports

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,17 +1,6 @@
 /* ANGULAR */
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-/* MATERIAL */
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCommonModule } from '@angular/material/core';
 /* COMPONENTS */
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
@@ -30,17 +19,6 @@ import { HomepageComponent } from './homepage/homepage.component';
   ],
   imports: [
     CommonModule,
-    FormsModule,
-    RouterModule,
-    ReactiveFormsModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatDividerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCommonModule,
     ComponentsModule
   ],
   exports: [
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 /* REPLACE IT WITH YOUR FIREBASE CONFIG */
 export const environment = {
   firebase: {
-    projectId: '',
-    appId: '',
-    storageBucket: '',
-    apiKey: '',
-    authDomain: '',
-    messagingSenderId: '',
-    measurementId: '',
+    projectId: 'angular-auth-template',
+    appId: '1:565031608751:web:bffe08c8157e4a37ba0f5a',
+    storageBucket: 'angular-auth-template.appspot.com',
+    apiKey: 'AIzaSyCLZGF-AgV5i6_v_ulu7DU2cvaXwFaU6yY',
+    authDomain: 'angular-auth-template.firebaseapp.com',
+    messagingSenderId: '565031608751',
+    measurementId: 'G-FQDRVSPN6Z',
   },
   production: true
 };
